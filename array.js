425e0ff2df88d92a910656d0d29750447fea18de
mokter <mdmokterhossain393@gmail.com>
2022-09-09 23:00:21 +0600

    array-practice

---
+++ array.js
@@ -0,0 +1,9 @@
+let allValue = [1, 4, 6, "samir", 9, "masum", "farazi", 10];
+allValue.push("emon");
+allValue.push("rafiq");
+allValue.pop();
+allValue.shift();
+allValue.unshift(15);
+allValue.reverse();
+allValue.join('-');
+console.log(allValue);
